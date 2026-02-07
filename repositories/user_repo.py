fc13570a62c06e174197ef30b3e9316a162107fc
ammu2244 <rakshithad819@gmail.com>
2026-02-07 14:29:42 +0530

    commit

--- repositories/user_repo.py
+++ repositories/user_repo.py
@@ -2,11 +2,12 @@ from models import User
 from sqlalchemy.orm import Session
 
 class UserRepo:
-    def __int__(self,db:Session):
+    def __init__(self,db:Session):
         self.db = db
     
     def add_user(self,user:User):
         self.db.add(user)
         self.db.commit()
         return user
-    
+    def get_user_by_email(self,email:str):
+        return self.db.query(User).filter(User.email == email).first()
